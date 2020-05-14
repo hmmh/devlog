dba5edcb7ddd1e90a8593ab4d44fe73cb4e108ff
Marco Zanter <Marco.Zanter@hmmh.de>
2020-05-14 12:39:25 +0200

    Fix Plugin Name

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,7 +5,7 @@ call_user_func(function () {
 
     // Register test plugin
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'devlog',
+        'Devlog.Devlog',
         'TestPlugin',
         'LLL:EXT:devlog/Resources/Private/Language/locallang.xlf:test_plugin',
         'EXT:devlog/Resources/Public/Images/ModuleIcon.svg'
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,16 +3,9 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-// Register the logging method with the appropriate hook
-//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_div.php']['devLog']['tx_devlog'] = \Devlog\Devlog\Utility\Logger::class . '->log';
-
-// Register log writers
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['devlog']['writers']['db'] = \Devlog\Devlog\Writer\DatabaseWriter::class;
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['devlog']['writers']['file'] = \Devlog\Devlog\Writer\FileWriter::class;
-
 // Register test plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Devlog.' . $_EXTKEY,
+    'Devlog.Devlog',
     'TestPlugin',
     ['TestPlugin' => 'index'],
     ['TestPlugin' => 'index']
@@ -21,7 +14,7 @@ if (!defined('TYPO3_MODE')) {
 $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'] = [
     \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
         // add Devlog Writers
-        'Devlog\\Devlog\\Writer\\DatabaseWriter' => [],
+        \Devlog\Devlog\Writer\DatabaseWriter::class => [],
         //'Devlog\\Devlog\\Writer\\FileWriter' => []
     ]
 ];
